6f3274dc60a1b3fa93655c6a1910e38ed91fc35a
AaronMan <aaron.ibt@gmail.com>
2019-04-22 23:30:54 +0200

    Code rules according to PEP 8

--- guessing_game.py
+++ guessing_game.py
@@ -3,42 +3,42 @@ Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
 """
-# IMPORTANT !!! : Verify to run it on Python 3.x 
-
 import random
 
-def start_game():
+invalid_message = "Invalid value! Please add ONLY numbers between 1 and 10:"
 
-    print("-" * 35 + "\nWelcome to the number guessing game!\n" + "-" * 35 + "\n") # Welcome message
-    score = []        # list that will store the scores
-    play_again = "y"  # loop control
+def start_game():
+    """Guess game logic"""
+    print("-"*35 + "\nWelcome to the number guessing game!\n" + "-"*35 +"\n")
+    score = []
+    play_again = "y"
 
     while play_again == "y":
-        answer = random.randint(1,10)     # Store random integer number to guess between 1 and 10
-        attemps_count = 0                 # Attemps counter
-        guess_number = None               # Null variable of guessing number
+        answer = random.randint(1,10)
+        attemps = 0
+        guess_number = None
 
         while guess_number != answer:
             try:
                 guess_number = int(input("Select a number between 1 and 10: "))
-                while (guess_number < 1) or (guess_number > 10): # Condition for correct range
-                    guess_number = int(input("Invalid value ! Please add ONLY numbers between 1 and 10: "))
-
-                attemps_count += 1
+                while (guess_number < 1) or (guess_number > 10):
+                # Condition to verify correct input range
+                    guess_number = int(input(invalid_message))
+                attemps += 1
                 if guess_number > answer:
                     print("=> It's lower")
                 elif guess_number < answer:
                     print("=> It's higher")
                 else:
-                    print("\nGot it ! It took you {} valid try(ies)\nEnd of game".format(attemps_count))
-                    score.append(attemps_count)
+                    print("\nGot it! It took you {} try(ies)".format(attemps))
+                    print("\nEnd of game")
+                    score.append(attemps)
                     play_again = input("Want to try again? [y]es/[n]o :")
                     if play_again.lower() == "n":
                         print("Bye")
                     else:
-                        print("\nThe BEST score is {}".format(min(score))) #get min value from the list
+                        print("\nThe BEST score is {}".format(min(score)))
                         continue
-
             # Handling exceptions
             except NameError as err:
                 print("Invalid value ! ({})\n Please try again".format(err))
@@ -47,6 +47,6 @@ def start_game():
             except ValueError as err:
                 print("Invalid value ! ({})\n Please try again".format(err))
 
+
 if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
     start_game()
